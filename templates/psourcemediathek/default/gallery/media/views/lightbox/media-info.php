cec5d92db9058da2db36d0887b7c46634c6d541f
Dernerd <dern3rd@outlook.at>
2022-03-15 22:40:55 +0100

    Galerie-Actions CSS Fix

--- templates/psourcemediathek/default/gallery/media/views/lightbox/media-info.php
+++ templates/psourcemediathek/default/gallery/media/views/lightbox/media-info.php
@@ -25,8 +25,8 @@ $media = psmt_get_current_media();
 		<div class="psmt-lightbox-action-links">
 			<?php do_action( 'psmt_lightbox_media_action_before_link', $media );?>
 			<?php if ( psmt_user_can_edit_media( psmt_get_media_id() ) ) : ?>
-                <a class="psmt-lightbox-media-action-link psmt-lightbox-edit-media-link" href="#" data-psmt-media-id="<?php psmt_media_id();?>"><?php _ex('Edit', 'lightbox edit media edit action label', 'psourcemediathek' );?> </a>
-                <a class="psmt-lightbox-media-action-link psmt-lightbox-edit-media-cancel-link" href="#" data-psmt-media-id="<?php psmt_media_id();?>"><?php _ex('Cancel', 'lightbox edit media cancel action label', 'psourcemediathek' );?></a>
+                <a class="psmt-lightbox-media-action-link psmt-lightbox-edit-media-link" href="#" data-psmt-media-id="<?php psmt_media_id();?>"><?php _ex('Bearbeiten', 'lightbox edit media edit action label', 'psourcemediathek' );?> </a>
+                <a class="psmt-lightbox-media-action-link psmt-lightbox-edit-media-cancel-link" href="#" data-psmt-media-id="<?php psmt_media_id();?>"><?php _ex('Abbrechen', 'lightbox edit media cancel action label', 'psourcemediathek' );?></a>
 			<?php endif;?>
 			<?php do_action( 'psmt_lightbox_media_action_after_link', $media );?>
         </div>
